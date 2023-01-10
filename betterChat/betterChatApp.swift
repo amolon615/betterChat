2d40e0ad5179f4d9fb84de1a76e15158d1247f32
amolon615 <114776102+amolon615@users.noreply.github.com>
2023-01-10 09:58:41 +0100

    Initial Commit

---
+++ betterChat/betterChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterChatApp.swift
+//  betterChat
+//
+//  Created by Artem on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct betterChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
